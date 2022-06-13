86277efb2adff81eeff4512529286cc99299e1e4
Evan Zhang <evan.zhang@redhat.com>
2022-06-13 17:36:32 -0400

    init

---
+++ src/main/java/com/redhat/demo/camel/HelloCamel.java
@@ -0,0 +1,18 @@
+package com.redhat.demo.camel;
+
+// camel-k: language=java
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class HelloCamel extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        // Write your routes here, for example:
+        from("timer:java?period={{time:1000}}").routeId("java")
+            .setBody()
+                .simple("Hello Camel from ${routeId}")
+            .log("${body}");
+    }
+}
